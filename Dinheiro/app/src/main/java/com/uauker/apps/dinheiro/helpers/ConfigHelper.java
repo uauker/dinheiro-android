5e37a930730dc67df3aa6e484374277955b2c77a
Paulo Guilherme <uauker@gmail.com>
2015-05-02 15:34:17 -0300

    bolsa de valores

--- Dinheiro/app/src/main/java/com/uauker/apps/dinheiro/helpers/ConfigHelper.java
+++ Dinheiro/app/src/main/java/com/uauker/apps/dinheiro/helpers/ConfigHelper.java
@@ -6,4 +6,6 @@ public interface ConfigHelper {
     public static final String CURRENCY = "http://m.app2.uol.com.br/aplicativo/iphone/cotacoes/v1/uol_cot_iphone_currency_showcase.xml";
 
     public static final String INDEXES = "http://m.app2.uol.com.br/aplicativo/iphone/cotacoes/v1/uol_cot_iphone_indices_values.xml";
+
+    public static final String STOCKS = "http://m.app2.uol.com.br/aplicativo/iphone/cotacoes/v1/uol_cot_iphone_items_summary.xml?codes=BBAS3.SA,OGXP3.SA,PETR3.SA,PETR4.SA,VALE3.SA,VALE5.SA";
 }

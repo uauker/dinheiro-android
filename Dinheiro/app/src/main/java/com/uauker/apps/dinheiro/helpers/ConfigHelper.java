a033aa1eaa18c500d8e73fdd6451e6d1d86ab4ab
Paulo Guilherme <uauker@gmail.com>
2015-05-02 15:31:52 -0300

    incluido indice e cotacao de valores

---
+++ Dinheiro/app/src/main/java/com/uauker/apps/dinheiro/helpers/ConfigHelper.java
@@ -0,0 +1,9 @@
+package com.uauker.apps.dinheiro.helpers;
+
+
+public interface ConfigHelper {
+
+    public static final String CURRENCY = "http://m.app2.uol.com.br/aplicativo/iphone/cotacoes/v1/uol_cot_iphone_currency_showcase.xml";
+
+    public static final String INDEXES = "http://m.app2.uol.com.br/aplicativo/iphone/cotacoes/v1/uol_cot_iphone_indices_values.xml";
+}

be33f74ebe16f6bd6046b6365afcf9f319676a11
Paulo Guilherme <uauker@gmail.com>
2015-05-03 16:42:32 -0300

    usando okhttp sobre request

--- Dinheiro/app/src/main/java/com/uauker/apps/dinheiro/fragments/ExchangeRateFragment.java
+++ Dinheiro/app/src/main/java/com/uauker/apps/dinheiro/fragments/ExchangeRateFragment.java
@@ -3,13 +3,24 @@ package com.uauker.apps.dinheiro.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 import com.uauker.apps.dinheiro.R;
+import com.uauker.apps.dinheiro.helpers.ConfigHelper;
 
-public class ExchangeRateFragment extends Fragment {
+import java.io.IOException;
+
+public class ExchangeRateFragment extends Fragment implements Callback {
+
+    OkHttpClient client = new OkHttpClient();
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -19,5 +30,36 @@ public class ExchangeRateFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        try {
+            run(ConfigHelper.CURRENCY);
+//            Log.e("ERROR ___----> ", content);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void run(String url) throws IOException {
+        final Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        final Call call = client.newCall(request);
+        call.enqueue(this);
+//        call.execute();
+
+//        final Response response = call.execute();
+//
+//        return response.body().string();
+    }
+
+    @Override
+    public void onFailure(Request request, IOException e) {
+
+    }
+
+    @Override
+    public void onResponse(Response response) throws IOException {
+        Log.e("apresentando resultado", response.body().string());
     }
 }

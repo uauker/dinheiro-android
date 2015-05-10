03f175e36cf303342e4d77bb8ade52ff7523cba6
Paulo Guilherme <uauker@gmail.com>
2015-05-10 12:20:49 -0300

    colocado CURRENCY_SPECFIC

--- Dinheiro/app/src/main/java/com/uauker/apps/dinheiro/helpers/ConfigHelper.java
+++ Dinheiro/app/src/main/java/com/uauker/apps/dinheiro/helpers/ConfigHelper.java
@@ -4,6 +4,7 @@ package com.uauker.apps.dinheiro.helpers;
 public interface ConfigHelper {
 
     String CURRENCY = "http://m.app2.uol.com.br/aplicativo/iphone/cotacoes/v1/uol_cot_iphone_currency_showcase.xml";
+    String CURRENCY_SPECFIC = "http://m.app2.uol.com.br/aplicativo/iphone/cotacoes/v1/uol_cot_iphone_currency_interday_table.xml?currency_code=USDC&start=9/2/2015&end=9/5/2015&chart_width=300";
 
     String INDEXES = "http://m.app2.uol.com.br/aplicativo/iphone/cotacoes/v1/uol_cot_iphone_indices_values.xml";
 
